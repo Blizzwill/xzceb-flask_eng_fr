bfabaa8e1064f414efc3a26547b77f8a5153e493
Blizzwill <blizzardwilliam@msn.com>
2022-12-31 22:04:07 -0700

    Final changes

---
+++ final_project/machinetraslation/translator.py
@@ -0,0 +1,46 @@
+"""
+Module to translate text using IBM Watson Traslator
+"""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """
+    Translates english text to french
+    """
+    if len(english_text) == 0:
+        return ''
+    response = language_translator.translate(
+        text=english_text,
+        model_id='en-fr'
+    ).get_result()
+    french_text = response['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    """
+    Translates french text to english
+    """
+    if len(french_text) == 0:
+        return ''
+    response = language_translator.translate(
+        text=french_text,
+        model_id='fr-en'
+    ).get_result()
+    english_text = response['translations'][0]['translation']
+    return english_text
